deacb6d62d63564d3effce732e3c8746758beb59
Vilmos Szabó <40409601+ShiroWorks@users.noreply.github.com>
2019-07-17 17:42:13 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ import axios from 'axios';
 const theme = createMuiTheme(themeObject);
 
 axios.defaults.baseURL =
-  'https://europe-west1-chibi-screams.cloudfunctions.net/api';
+  'your firebase functions api';
 
 const token = localStorage.FBIdToken;
 if (token) {
